5b32a27bc324d7c143e166556642a5470dbff3b2
dud-ka <kaardud@gmail.com>
2018-01-03 21:36:24 +0100

    fix makeNSennetnes function

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-var firstColumn = ["Koleżanki i koledzy", "Z drugiej strony", "Podobnie", "Nie zapominajmy jednak, że", 
-"W ten sposób", "Praktyka dnia codziennego dowodzi, że", "Wagi i znaczenia tych problemów nie trzeba szerzej uzasadniać, ponieważ", 
-"Różnorakie i bogate doświadczenia", "Troska organizacji, a szczególnie", "Wyższe założenia ideowe, a także" ];
-var secondColumn = ["realizacja nakreślonych zadań programowych", "zakres i miejsce szkolenia kadr",  
-"stały wzrost ilości i zakres naszej aktywności", "aktualna struktura organizacji", "nowy model działalności organizacyjnej", 
-"dalszy rozwój różnych form działalności", "stałe zabezpieczenie informacyjno-programowe naszej działalności", 
-"wzmacnianie i rozwijanie struktur", "konsultacja z szerokim aktywem", "rozpoczęcie powszechnej akcji kształtowania postaw"];
-var thirdColumn = ["zmusza nas do przeanalizowania", "spełnia istotną rolę w kształtowaniu", 
-"wymaga sprecyzowania i określenia", "pomaga w przygotowaniu i realizacji", "zabezpiecza udział szerokiej grupie w kształtowaniu", 
-"spełnia ważne zadania w wypracowaniu", "umożliwia w większym stopniu tworzenie", "powoduje docenianie wagi", 
-"przedstawia interesującą próbę sprawdzenia", "pociąga za sobą proces wdrażania i unowocześnienia"];
-var fourthColumn = ["istniejących warunków administracyjno-finansowych", "dalszych kierunków rozwoju", 
-"systemu powszechnego uczestnictwa", "postaw uczestników wobec zadań stawianych przez organizację", "nowych propozycji", 
-"kierunków postępowego wychowania", "systemu szkolenia kadry odpowiadającego potrzebom", "odpowiednich warunków aktywizacji", 
-"modelu rozwoju", "form oddziaływania"];
+var firstColumn = ["Koleżanki i koledzy", "Z drugiej strony"];//, "Podobnie", "Nie zapominajmy jednak, że", 
+//"W ten sposób", "Praktyka dnia codziennego dowodzi, że", "Wagi i znaczenia tych problemów nie trzeba szerzej uzasadniać, ponieważ", 
+//"Różnorakie i bogate doświadczenia", "Troska organizacji, a szczególnie", "Wyższe założenia ideowe, a także" ];
+var secondColumn = ["realizacja nakreślonych zadań programowych", "zakres i miejsce szkolenia kadr"];//,  
+//"stały wzrost ilości i zakres naszej aktywności", "aktualna struktura organizacji", "nowy model działalności organizacyjnej", 
+//"dalszy rozwój różnych form działalności", "stałe zabezpieczenie informacyjno-programowe naszej działalności", 
+//"wzmacnianie i rozwijanie struktur", "konsultacja z szerokim aktywem", "rozpoczęcie powszechnej akcji kształtowania postaw"];
+var thirdColumn = ["zmusza nas do przeanalizowania", "spełnia istotną rolę w kształtowaniu"];//, 
+//"wymaga sprecyzowania i określenia", "pomaga w przygotowaniu i realizacji", "zabezpiecza udział szerokiej grupie w kształtowaniu", 
+//"spełnia ważne zadania w wypracowaniu", "umożliwia w większym stopniu tworzenie", "powoduje docenianie wagi", 
+//"przedstawia interesującą próbę sprawdzenia", "pociąga za sobą proces wdrażania i unowocześnienia"];
+var fourthColumn = ["istniejących warunków administracyjno-finansowych", "dalszych kierunków rozwoju"];//, 
+//"systemu powszechnego uczestnictwa", "postaw uczestników wobec zadań stawianych przez organizację", "nowych propozycji", 
+//"kierunków postępowego wychowania", "systemu szkolenia kadry odpowiadającego potrzebom", "odpowiednich warunków aktywizacji", 
+//"modelu rozwoju", "form oddziaływania"];
 
 
 // 1 makeSentencePart - create part of sentence
@@ -29,19 +29,19 @@ function makeSentence() {
 //3 makeNSentences - create N sentences in for loop with "N" parameters
 // sentences will be string array and you sholuld check if sentence exist in array (if exist don't add it to aaray)
 
-function makeNSentences() {
-    var sentencesNumber = 5;
-    var start = 0;
-    var sentence = "";
-    while (start < sentencesNumber) {
-       sentence +=  makeSentence();
-       start++;
-   };
-   return sentence;
+function makeNSentences(sentencesNumber) {
+    var sentencesArray = [];
+    while (sentencesArray.length < sentencesNumber) {
+        var sentence = makeSentence();
+        if (!sentencesArray.includes(sentence)) {
+            sentencesArray.push(sentence);
+        };
+    };
+    return sentencesArray;
 };
 
 
-console.log(makeNSentences());
+console.log(makeNSentences(16));
 
 
 
